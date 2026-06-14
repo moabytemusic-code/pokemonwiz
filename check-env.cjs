282543c6c498a3950427bc5c582f1f9003d2368b
kmtwarrior <kmtwarrior@kmtwarriors-Mac-mini.local>
2026-06-14 07:25:58 -0400

    chore: remove debug file

--- check-env.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const envText = fs.readFileSync('/Users/kmtwarrior/projects2026/pokemon-wiz/.env.local', 'utf-8');
-// Just print the keys without the values
-const lines = envText.split('\n').filter(l => l.trim() && !l.startsWith('#'));
-for (const line of lines) {
-  const [key, val] = line.split('=');
-  console.log(key + '=' + (val ? val.substring(0, 15) + '...' : ''));
-}
